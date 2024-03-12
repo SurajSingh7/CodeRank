3a676e74f97195451c98742c25ba54d8d6e2b30b
SurajSingh7 <220suraj@gmail.com>
2024-03-12 12:28:37 +0530

    fix issue

--- src/services/apis.js
+++ src/services/apis.js
@@ -1,5 +1,4 @@
-// const BASE_URL = "http://localhost:4000/api/v1"
-
+// const BASE_URL = "http://localhost:4000/api/v1",
 const BASE_URL = "https://upskills-backend.vercel.app/api/v1";
 
 // AUTH ENDPOINTS
